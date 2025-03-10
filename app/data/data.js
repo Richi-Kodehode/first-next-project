4750b0331ce191c5032e80b7364e26dceeea4f3b
Richi-Kodehode <richard25sfj@gmail.com>
2025-03-10 15:00:42 +0100

    who knows

---
+++ app/data/data.js
@@ -0,0 +1,24 @@
+export const altProductData = [
+  {
+    id: 1,
+    name: "Sony Alpha A7 III",
+    brand: "Sony",
+    price: 1999.99,
+    description:
+      "A professional-grade mirrorless camera with superb image quality.",
+    stock: 20,
+    image: "/images/sony_a7iii.jpeg",
+    ref: "sony_a7iii",
+  },
+  {
+    id: 2,
+    name: "Sony FX3 Cinema Line",
+    brand: "Sony",
+    price: 3899.99,
+    description:
+      "A full-frame cinema camera designed for professional filmmakers.",
+    stock: 10,
+    image: "/images/sony_fx3.webp",
+    ref: "sony_fx3",
+  },
+];
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Figtree, Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const figtree = Figtree({
   subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const roboto = Roboto({
   subsets: ["latin"],
 });
 
@@ -19,10 +16,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${figtree.variable} ${roboto.variable} antialiased`}>
+        <main>{children}</main>
       </body>
     </html>
   );
